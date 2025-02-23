820c7caab8016ead6f41eef89e9998fbe4c79aa2
acmerev <acmerev@gmail.com>
2025-02-23 13:16:45 -0600

    Proyect chat and deepseek in spring boot by Emmanuel MC from  indigo

---
+++ deepseekchat/deepseekchat/src/main/java/com/indigo/deepseekchat/DeepseekchatApplication.java
@@ -0,0 +1,13 @@
+package com.indigo.deepseekchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepseekchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepseekchatApplication.class, args);
+	}
+
+}
